7128b9f18d1eccef7b120b29c79d28be95080ee2
fpudo <felipepudo@gmail.com>
2017-07-29 22:34:59 -0300

    CSS Finished

--- src/App.js
+++ src/App.js
@@ -6,24 +6,254 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="NavBar"></nav>
+        <header className="NavBar">
+          <div>
+            <h1 className="Logo">Business Canvas</h1>
+          </div>
+        </header>
         <section className="Canvas">
           <section className="How">
-            <div className="KeyPartners BoxStyle">KeyPartners</div>
-            <div className="KeyActivities BoxStyle">KeyActivities</div>
-            <div className="KeyResources BoxStyle">KeyResources</div>
+            <div className="KeyPartners BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Key Partners</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 3</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+            <div className="KeyActivities BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Key Activities</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 3</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+            <div className="KeyResources BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Key Resources</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 3</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
           </section>
           <section className="What">
-            <div className="ValueProposition BoxStyle">Value Proposition</div>
+            <div className="ValueProposition BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Value Proposition</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 3</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
           </section>
           <section className="ToWho">
-            <div className="CustomerRelationship BoxStyle">CustomerRelationship</div>
-            <div className="CustomerSegments BoxStyle">CustomerSegments</div>
-            <div className="Channels BoxStyle">Channels</div>
+            <div className="CustomerRelationship BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Customer Relationship</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 3</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+            <div className="CustomerSegments BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Customer Segments</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 3</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+            <div className="Channels BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Channels</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 3</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
           </section>
           <section className="HowMuch">
-            <div className="CostStructure BoxStyle">CostStructure</div>
-            <div className="RevenueStreams BoxStyle">RevenueStreams</div>
+            <div className="CostStructure BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Cost Structure</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
+            <div className="RevenueStreams BoxStyle">
+              <div className="BoxHeader">
+                <div className="IconWrapper">
+                  <i className="fa fa-link fa-lg icon"></i>
+                </div>
+                <div className="TitleWrapper">
+                  <h2 className="BoxTitle">Revenue Streams</h2>
+                </div>
+                <div className="AddWrapper">
+                  <button className="AddItem">+</button>
+                </div>
+              </div>
+              <div className="BoxContent">
+                <div>
+                  <h3 className="ItemTitle">Partner 1</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div>
+                  <h3 className="ItemTitle">Partner 2</h3>
+                  <p className="KeyDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </div>
           </section>
         </section>
       </div>
